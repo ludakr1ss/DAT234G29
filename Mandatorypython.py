0008507a67bd96da4aa1ae2886a9786cdd90e9a4
ludakr1ss <awesome.krydder@gmail.com>
2019-10-08 16:31:53 +0200

    Update Mandatorypython.py

--- Mandatorypython.py
+++ Mandatorypython.py
@@ -3,13 +3,17 @@ import re
 
 
 def task_1():
-
     html = urlopen("https://links.datapor.no/")
     content = html.read()
     print(content)
 
+
 def task_2():
-    
+    r = urlopen("https://links.datapor.no/")
+    http = re.findall('^http?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', r.text)
+    return http
+    https = re.findall('^https?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', r.text)
+    return https
 
 
 if __name__ == "__main__":
