ec205f1bb0c9781555e5c6dc166e09d5d70260dc
felpus <35775929+felpus@users.noreply.github.com>
2019-10-11 23:00:26 +0200

    Update pythonDeliverable.py

--- pythonDeliverable.py
+++ pythonDeliverable.py
@@ -1,8 +1,5 @@
 import requests
 import re
-from tld import get_tld
-from urlextract import URLExtract
-import tldextract
 
 def task_1():
     r = requests.get("https://links.datapor.no/")
@@ -16,23 +13,20 @@ def task_2():
     linksHttps = re.findall('https://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', r.text)
     for link in linksHttp:
         counterHttp+=1
+        trash, link2 = link.split("//")
+        print(link2)
     for link in linksHttps:
         counterHttps+=1
+        print(link)
     print("Number of http links:",counterHttp)
     print("Number of https links:",counterHttps)
 
 
 def task_3():
-    tldr = requests.get("https://links.datapor.no/")
+    r = requests.get("https://links.datapor.no/")
     pattern = '/(?:[a-zA-Z]*\.)+([a-zA-Z]+)(?:\/.*)?'
-    tlds = re.findall(pattern, tldr.text)
-    myList = tlds
-    auxiliaryList = []
-    for word in myList:
-       if word not in auxiliaryList:
-           auxiliaryList.append(word)
-           auxiliaryList = list(set(myList))
-           print(list(set(myList)))
+    tlds = re.findall(pattern, r.text)
+    print(tlds)
 
 def task_4():
     r = requests.get("https://links.datapor.no/")
@@ -49,7 +43,6 @@ def task_4():
             linkList.append(link)
             trash, link2 = link.split("//")
             print(link2)
-
 
 def task_5():
     r = requests.get("https://links.datapor.no/")
