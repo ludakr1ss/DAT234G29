fb95925ff54550fad8318cf111b389fb7129fb7a
Kristian Jul-Larsen <krisju15@uia.no>
2019-10-11 16:58:49 +0200

    med task 3 og 4

--- pythonDeliverable.py
+++ pythonDeliverable.py
@@ -1,6 +1,8 @@
 import requests
 import re
-
+from tld import get_tld
+from urlextract import URLExtract
+import tldextract
 
 def task_1():
     r = requests.get("https://links.datapor.no/")
@@ -20,12 +22,25 @@ def task_2():
     print("Number of https links:",counterHttps)
 
 
-
 def task_3():
-    test
+    r = requests.get("https://links.datapor.no/")
+    result = re.findall(r"\w+://\w+\.\w+\.\w+/?[\w\.\?=#]*", r.text)
+    urls = result
+    for url in urls:
+        info = get_tld(url, as_object=True)
+        print("Suffix: ", info.suffix)
+
+def task_4():
+    r = requests.get("https://links.datapor.no/")
+    result = re.findall(r"\w+://\w+\.\w+\.\w+/?[\w\.\?=#]*", r.text)
+    urls = result
+    for url in urls:
+        info = get_tld(url, as_object=True)
+        print("Suffix: ", info.domain)
 
 
 if __name__ == "__main__":
     task_1()
     task_2()
     task_3()
+    task_4()
