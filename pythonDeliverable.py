9212c50f3fbe2f059275cb96aa9ebe8b3eb61e0d
ludakr1ss <35729206+ludakr1ss@users.noreply.github.com>
2019-10-11 22:55:55 +0200

    Update pythonDeliverable.py

--- pythonDeliverable.py
+++ pythonDeliverable.py
@@ -79,9 +79,9 @@ def task_6():
 
 
 if __name__ == "__main__":
-    #task_1()
-    #task_2()
-    #task_3()
-    #task_4()
-    #task_5()
+    task_1()
+    task_2()
+    task_3()
+    task_4()
+    task_5()
     task_6()
